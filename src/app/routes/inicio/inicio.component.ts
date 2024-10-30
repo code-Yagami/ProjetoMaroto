085f8eb6d03da849dd82060b5c0c337685efa4fa
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-10-30 19:07:48 -0300

    feat: desenvolve o corpo e estilo do navbar

--- src/app/routes/inicio/inicio.component.ts
+++ src/app/routes/inicio/inicio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss'
 })
