d40e9048ff739879bf0e1cf63fb65dfe72cd0a51
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-10-31 13:37:00 -0300

    fix: corrige estilo do navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { BodyComponent } from "./components/body/body.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, BodyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
