8a33b3b6c25b95c9656716d282c7b0f125d3b8aa
Leonardo Aguiar <leonardo.aguiar@pdcase.com.br>
2024-11-04 16:13:14 -0300

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,15 @@
-<<<<<<< HEAD
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+
 import { LoginComponent } from "./components/login/login.component";
 import { InicioComponent } from "./routes/inicio/inicio.component";
+import { SenhaComponent } from "./routes/administracao-acesso/senha/senha.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: 'pderp/login', pathMatch: 'full' },
   { path: 'pderp/login', component: LoginComponent },
   { path: 'pderp/inicio', component: InicioComponent },
-=======
-import { Routes } from '@angular/router';
-import { InicioComponent } from './routes/inicio/inicio.component';
-import { SenhaComponent } from './routes/administracao-acesso/senha/senha.component';
-
-export const routes: Routes = [
-  {
-    path: 'pderp/inicio',
-    component: InicioComponent,
-  },
-  {
-    path: 'pderp/acesso/senha',
-    component: SenhaComponent
-  }
->>>>>>> origin/feat/tela-inicio-marcos
+  { path: 'pderp/acesso/senha', component: SenhaComponent }
 ];
 
 @NgModule({
