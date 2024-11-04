6b3dcf52f4a6cdb59b1023859ba6b70d94dd6dc8
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-11-04 09:38:26 -0300

    feat: cria e configura rota de alterar senha

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './routes/inicio/inicio.component';
+import { SenhaComponent } from './routes/senha/senha.component';
 
 export const routes: Routes = [
   {
     path: 'pderp/inicio',
     component: InicioComponent,
   },
+  {
+    path: 'pderp/acesso/senha',
+    component: SenhaComponent
+  }
 ];
