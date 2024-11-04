0803d217497dc6f5712a9b9203ff453ada15bfcf
Leonardo Aguiar <leonardo.aguiar@pdcase.com.br>
2024-11-04 17:33:09 -0300

    Criado Formulários com agular e bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'start-front-angular';
+  title = 'Meu App Angular';
+
+  description = 'Este é um exemplo de componente Angular.';
+
+  showMessage(): void {
+    alert('Bem-vindo ao meu app Angular!');
+  }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
+  constructor(private readonly fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      login: ['', Validators.required],
+      senhaAtual: ['', Validators.required],
+      novaSenha: ['', Validators.required],
+      confirmarNovaSenha: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const formData = this.loginForm.value;
+        console.log(this.loginForm.value);
+        console.log('Dados do Formulário:', formData);
+
+    } else {
+
+      this.loginForm.markAllAsTouched();
+    }
+  }
 }
+
