3213b3deaaae4acc4cc32e79e4da50d74b198b3b
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-11-04 15:44:30 -0300

    fix, correção layout

--- src/app/routes/administracao-acesso/senha/senha.component.ts
+++ src/app/routes/administracao-acesso/senha/senha.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../../components/header/header.component";
 
 @Component({
   selector: 'app-senha',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './senha.component.html',
   styleUrl: './senha.component.scss'
 })
--- src/app/routes/inicio/inicio.component.ts
+++ src/app/routes/inicio/inicio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/header/header.component";
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss'
 })
