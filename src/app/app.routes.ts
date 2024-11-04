02caee6d9c26af706a89c75d7527baa917df25e8
Paulo Martins <paulomart90@gmail.com>
2024-11-04 15:55:13 -0300

    feat: Implementação da funcionalidade do botão "Entrar" e mensagem de erro de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './routes/inicio/inicio.component';
 import { SenhaComponent } from './routes/administracao-acesso/senha/senha.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'pderp/login', pathMatch: 'full' },
+  { path: 'pderp/login', component: LoginComponent },
+
   {
     path: 'pderp/inicio',
     component: InicioComponent,
--- src/app/routes/inicio/inicio.component.ts
+++ src/app/routes/inicio/inicio.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss'
 })
