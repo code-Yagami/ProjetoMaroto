ab0b1cb547f339cab389a0071296b842c411fc35
Paulo Martins <paulomart90@gmail.com>
2024-11-04 10:29:55 -0300

    feat: Implementação e inclusão de rota do component card

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,19 @@
-import {NgModule} from "@angular/core";
-import {RouterModule, Routes} from '@angular/router';
-import {LoginComponent} from "./components/login/login.component";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from "./components/login/login.component";
+import { SharedModule } from "./shared/shared.module";
 
 export const routes: Routes = [
   { path: '', redirectTo: 'pderp/login', pathMatch: 'full' },
   { path: 'pderp/login', component: LoginComponent }
 ];
 
-@NgModule({ imports: [RouterModule.forRoot(routes)], exports: [RouterModule] })
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes),
+    SharedModule
+  ],
+  exports: [RouterModule]
+})
 export class AppRoutingModule { }
+
