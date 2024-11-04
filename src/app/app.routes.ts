fa876bb946e7fcedf929f7e3e7ac755e248c3cbe
Leonardo Aguiar <leonardo.aguiar@pdcase.com.br>
2024-11-04 09:08:50 -0300

    fix no package-look, style.scss e no app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,16 @@
-<<<<<<< HEAD
-import { Routes } from '@angular/router';
-import { InicioComponent } from './routes/inicio/inicio.component';
-
-export const routes: Routes = [
-  {
-    path: 'pderp/inicio',
-    component: InicioComponent,
-  },
-];
-=======
-import {NgModule} from "@angular/core";
-import {RouterModule, Routes} from '@angular/router';
-import {LoginComponent} from "./components/login/login.component";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./components/login/login.component";
+import { InicioComponent } from "./routes/inicio/inicio.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: 'pderp/login', pathMatch: 'full' },
-  { path: 'pderp/login', component: LoginComponent }
+  { path: 'pderp/login', component: LoginComponent },
+  { path: 'pderp/inicio', component: InicioComponent },
 ];
 
-@NgModule({ imports: [RouterModule.forRoot(routes)], exports: [RouterModule] })
-export class AppRoutingModule { }
->>>>>>> origin/feat/tela-login-leonardo
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
