9c42f425afb4ea31b18b73ef71f37b425c000fea
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-11-04 09:57:49 -0300

    fix: ajusta a organização das rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './routes/inicio/inicio.component';
-import { SenhaComponent } from './routes/senha/senha.component';
+import { SenhaComponent } from './routes/administracao-acesso/senha/senha.component';
 
 export const routes: Routes = [
   {
