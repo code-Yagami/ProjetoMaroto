b926c704a7d9bb54556c319dd982e4125d83d8c6
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-11-05 10:56:39 -0300

    fix: corrige erro no modulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Meu App Angular';
-
   description = 'Este é um exemplo de componente Angular.';
 
   showMessage(): void {
     alert('Bem-vindo ao meu app Angular!');
   }
 }
+
