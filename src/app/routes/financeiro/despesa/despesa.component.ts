43fb0cacf7bb327928d8bf30870f7a77fb9e9be2
Leandro Ribeiro <leandro.ribeiro@pdcase.com.br>
2024-11-06 15:01:51 -0300

    feat: adicionando formulário da tela de desepesas

--- src/app/routes/financeiro/despesa/despesa.component.ts
+++ src/app/routes/financeiro/despesa/despesa.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../../components/header/header.component';
 import { SharedModule } from "../../../shared/shared.module";
 
 @Component({
   selector: 'app-despesa',
   standalone: true,
-  imports: [HeaderComponent, SharedModule],
+  imports: [HeaderComponent, SharedModule, FormsModule, CommonModule],
   templateUrl: './despesa.component.html',
   styleUrl: './despesa.component.scss'
 })
 export class DespesaComponent {
+  dataInicio: Date | null = null;
+  dataFinal: Date | null = null;
+  numero: number | null = null;
+  status: string | null = '';
+  colaborador: string | null = '';
+
+  statusOptions = [
+    'Redigido',
+    'Autorizado coordenador',
+    'Autorizado Gerência',
+    'Autorizado Gerência regional',
+    'Aprovado para pagamento',
+    'Pago',
+    'Reprovada',
+    'Todos'
+  ];
 
 }
