cc2f0d34c659f04458085be3dfff632d45ae3231
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-11-08 14:56:57 -0300

    feat: cria rota de adiantamentos

---
+++ src/app/routes/pagamentos/adiantamentos/adiantamentos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adiantamentos',
+  standalone: true,
+  imports: [],
+  templateUrl: './adiantamentos.component.html',
+  styleUrl: './adiantamentos.component.scss'
+})
+export class AdiantamentosComponent {
+
+}
