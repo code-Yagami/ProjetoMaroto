0821919358b95a5c1daec22470996aa3096f5c9a
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-11-08 17:20:58 -0300

    feat: cria estrutura do textarea-component

--- src/app/components/form-components/textarea-component/textarea-component.component.ts
+++ src/app/components/form-components/textarea-component/textarea-component.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-textarea-component',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './textarea-component.component.scss'
 })
 export class TextareaComponentComponent {
-
+  @Input() idInput: string = '';
+  @Input() nameInput: string = '';
+  @Input() placeholderInput: string = '';
 }
