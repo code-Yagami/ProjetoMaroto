91cd662af1e78437617fb4b21d12721c4496e052
Paulo Martins <paulomart90@gmail.com>
2024-11-08 16:03:06 -0300

    feat: implementação do botão Novo

---
+++ src/app/routes/financeiro/despesa/modal/novo-despesa/novo-despesa.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-novo-despesa',
+  standalone: true,
+  imports: [],
+  templateUrl: './novo-despesa.component.html',
+  styleUrl: './novo-despesa.component.scss'
+})
+export class NovoDespesaComponent {
+
+}
