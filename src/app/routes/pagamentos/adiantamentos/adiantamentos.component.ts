b3eaa76c71bba2d3bdf36acf45be2052eccbac71
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-11-11 15:39:55 -0300

    feat: implementa a mensagem de alerta em adiantamentos

--- src/app/routes/pagamentos/adiantamentos/adiantamentos.component.ts
+++ src/app/routes/pagamentos/adiantamentos/adiantamentos.component.ts
@@ -1,18 +1,32 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../../components/header/header.component";
-import { ButtonComponentComponent } from "../../../components/form-components/button-component/button-component.component";
-import { ModalFormComponent } from '../../../components/form-components/modal-form/modal-form.component'
-import { InputComponentComponent } from "../../../components/form-components/input-component/input-component.component";
-import { TextareaComponentComponent } from "../../../components/form-components/textarea-component/textarea-component.component";
+import { HeaderComponent } from '../../../components/header/header.component';
+import { ButtonComponentComponent } from '../../../components/form-components/button-component/button-component.component';
+import { ModalFormComponent } from '../../../components/form-components/modal-form/modal-form.component';
+import { InputComponentComponent } from '../../../components/form-components/input-component/input-component.component';
+import { TextareaComponentComponent } from '../../../components/form-components/textarea-component/textarea-component.component';
+import { CommonModule } from '@angular/common';
+import { AlertComponent } from "../../../components/alert/alert.component";
 
 @Component({
   selector: 'app-adiantamentos',
   standalone: true,
-  imports: [HeaderComponent, ButtonComponentComponent, ModalFormComponent, InputComponentComponent, TextareaComponentComponent],
+  imports: [
+    HeaderComponent,
+    ButtonComponentComponent,
+    ModalFormComponent,
+    InputComponentComponent,
+    TextareaComponentComponent,
+    CommonModule,
+    AlertComponent
+],
   templateUrl: './adiantamentos.component.html',
-  styleUrl: './adiantamentos.component.scss'
+  styleUrl: './adiantamentos.component.scss',
 })
 export class AdiantamentosComponent {
+  mostrarMensagem: boolean = false;
+  tipoAlerta!: string;
+  textoMensagem!: string;
+  testeAlerta: number = 1;
 
   isModalVisible = false;
 
@@ -25,7 +39,20 @@ export class AdiantamentosComponent {
   }
 
   onModalConfirm() {
-    // Lógica a ser executada ao confirmar o modal
     console.log('Modal confirmado!');
+
+    // teste da mensagem de alerta
+    if (this.testeAlerta == 1) {
+      this.tipoAlerta = 'sucesso';
+      this.textoMensagem = 'Salvo com sucesso!';
+    } else {
+      this.tipoAlerta = 'erro';
+      this.textoMensagem = 'Erro ao salvar!';
+    }
+
+    this.mostrarMensagem = true;
+    setTimeout(() => {
+      this.mostrarMensagem = false;
+    }, 3000);
   }
 }
