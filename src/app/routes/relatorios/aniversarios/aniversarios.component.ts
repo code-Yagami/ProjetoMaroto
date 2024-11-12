a4c01093df3fed8430f71cafea39a36ea77f78ce
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-11-12 16:41:47 -0300

    feat: cria rota de relatorios/aniversarios

---
+++ src/app/routes/relatorios/aniversarios/aniversarios.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aniversarios',
+  standalone: true,
+  imports: [],
+  templateUrl: './aniversarios.component.html',
+  styleUrl: './aniversarios.component.scss'
+})
+export class AniversariosComponent {
+
+}
