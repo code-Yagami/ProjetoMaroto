8ba75acd177070c3a41e99c50139edef761f4761
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-11-12 16:49:37 -0300

    feat: estrutura aniversarios para o padrão

--- src/app/routes/relatorios/aniversarios/aniversarios.component.ts
+++ src/app/routes/relatorios/aniversarios/aniversarios.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../../components/header/header.component";
 
 @Component({
   selector: 'app-aniversarios',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './aniversarios.component.html',
   styleUrl: './aniversarios.component.scss'
 })
