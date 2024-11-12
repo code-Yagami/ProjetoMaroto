7af68611d6fec914aa23e5168a3d51d453884748
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-11-12 20:39:30 -0300

    feat: estrutura o select-component

--- src/app/components/form-components/select-component/select-component.component.ts
+++ src/app/components/form-components/select-component/select-component.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-select-component',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './select-component.component.html',
   styleUrl: './select-component.component.scss'
 })
 export class SelectComponentComponent {
+  @Input() listaRender!: string[] | null;
+  @Output() seleciona = new EventEmitter<string>();
 
+  onSelecionado(event: Event) {
+    const valorSelecionado = (event.target as HTMLSelectElement).value;
+    this.seleciona.emit(valorSelecionado);
+  }
 }
