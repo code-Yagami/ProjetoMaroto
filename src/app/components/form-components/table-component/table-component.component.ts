5410721a96c0fc74fbc9700e5d3895969619b0af
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-11-13 11:08:56 -0300

    feat: estrutura o table-component

--- src/app/components/form-components/table-component/table-component.component.ts
+++ src/app/components/form-components/table-component/table-component.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-table-component',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './table-component.component.html',
   styleUrl: './table-component.component.scss'
 })
 export class TableComponentComponent {
-
+  @Input() dados: any[] = [];
 }
