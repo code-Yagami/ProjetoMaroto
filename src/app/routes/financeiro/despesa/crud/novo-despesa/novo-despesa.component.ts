dcfdd7899dbabb4f4083465dd404a5d57e346f3c
Paulo Martins <paulomart90@gmail.com>
2024-11-13 15:43:15 -0300

    feat: tela de "novo"

--- src/app/routes/financeiro/despesa/crud/novo-despesa/novo-despesa.component.ts
+++ src/app/routes/financeiro/despesa/crud/novo-despesa/novo-despesa.component.ts
@@ -1,21 +1,30 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from '../../../../../components/header/header.component';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-import { SharedModule } from '../../../../../shared/shared.module';
-import { InputComponentComponent } from "../../../../../components/form-components/input-component/input-component.component";
-import { ButtonComponentComponent } from "../../../../../components/form-components/button-component/button-component.component";
+
+import { ButtonComponentComponent } from '../../../../../components/form-components/button-component/button-component.component';
+import { InputComponentComponent } from '../../../../../components/form-components/input-component/input-component.component';
+import { HeaderComponent } from '../../../../../components/header/header.component';
+import { ModalFormComponent } from "../../../../../components/form-components/modal-form/modal-form.component";
 
 @Component({
   selector: 'app-novo-despesa',
   standalone: true,
-  imports: [HeaderComponent, SharedModule, FormsModule, CommonModule, RouterLink, NovoDespesaComponent, InputComponentComponent, ButtonComponentComponent],
+  imports: [
+    HeaderComponent,
+    FormsModule,
+    CommonModule,
+    RouterLink,
+    NovoDespesaComponent,
+    InputComponentComponent,
+    ButtonComponentComponent,
+    ModalFormComponent
+],
   templateUrl: './novo-despesa.component.html',
-  styleUrl: './novo-despesa.component.scss'
+  styleUrl: './novo-despesa.component.scss',
 })
 export class NovoDespesaComponent {
-
   dataInicio: Date | null = null;
   dataFinal: Date | null = null;
   numero: number | null = null;
@@ -30,6 +39,25 @@ export class NovoDespesaComponent {
     'Autorizado Gerência regional',
     'Aprovado para pagamento',
     'Pago',
-    'Reprovada'
+    'Reprovada',
   ];
+
+  categoriaOptions =[
+    'Administrativas',
+    'Alimentação',
+    'Aluguel',
+    'Bancárias',
+    'Luz',
+    'Marketing',
+    'Participação de lucro',
+    'Patrimonial',
+    'Pessoal',
+    'Seguro/Previdência',
+    'Serviços Extras',
+    'Telefone',
+    'Transporte',
+    'Treinamentos',
+    'Tributárias',
+    'Viagens'
+  ]
 }
