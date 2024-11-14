1a3b3586a9da7ba628b38fe132916d16fcc84960
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-11-14 10:25:39 -0300

    feat: cria componente data-component

---
+++ src/app/components/form-components/data-component/data-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './data-component.component.html',
+  styleUrl: './data-component.component.scss'
+})
+export class DataComponentComponent {
+
+}
