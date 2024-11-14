dc4feae1054ecab81282ae76b055a8921eb67609
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-11-14 14:27:02 -0300

    feat: estrutura componente de data

--- src/app/components/form-components/data-component/data-component.component.ts
+++ src/app/components/form-components/data-component/data-component.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Importar FormsModule
 
 @Component({
   selector: 'app-data-component',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './data-component.component.html',
   styleUrl: './data-component.component.scss'
 })
 export class DataComponentComponent {
+  dateValue: string = ''; // Variável para armazenar o valor da data
 
+  // Criando um EventEmitter para emitir a data para o componente pai
+  @Output() dateChange = new EventEmitter<string>();
+
+  // Método para enviar o valor da data para o componente pai
+  getDate() {
+    this.dateChange.emit(this.dateValue); // Emitir o valor da data para o componente pai
+  }
 }
