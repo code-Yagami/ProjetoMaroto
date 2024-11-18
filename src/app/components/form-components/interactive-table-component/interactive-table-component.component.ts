43e06063917ffaca8dbb32351651c7e8f508ea7f
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-11-18 10:04:29 -0300

    feat: cria interactive-table-component

---
+++ src/app/components/form-components/interactive-table-component/interactive-table-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interactive-table-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './interactive-table-component.component.html',
+  styleUrl: './interactive-table-component.component.scss'
+})
+export class InteractiveTableComponentComponent {
+
+}
