a3b470ae18faaf00eee444c5c23a4253a2aa3a84
Paulo Martins <paulomart90@gmail.com>
2024-11-21 12:37:13 -0300

    fix: alteração na estrutura da tela.

--- src/app/router/financeiro/despesa/despesa.component.ts
+++ src/app/router/financeiro/despesa/despesa.component.ts
@@ -3,14 +3,39 @@ import { LayoutComponent } from "../../../components/layout/layout.component";
 import { RouterLink } from '@angular/router';
 import { CardComponent } from "../../../components/form-components/card/card.component";
 import { TableComponent } from '../../../components/form-components/table/table.component';
+import { NovaDespesaComponent } from './modals/nova-despesa/nova-despesa.component';
 
 @Component({
   selector: 'app-despesa',
   standalone: true,
-  imports: [LayoutComponent, RouterLink, CardComponent, TableComponent],
+  imports: [LayoutComponent, RouterLink, CardComponent, TableComponent, NovaDespesaComponent],
   templateUrl: './despesa.component.html',
   styleUrl: './despesa.component.scss'
 })
 export class DespesaComponent {
+
+  dataInicio: Date | null = null;
+  dataFinal: Date | null = null;
+  numero: number | null = null;
+  status: string | null = '';
+  colaborador: string | null = '';
+
+  statusOptions = [
+    'Todos',
+    'Redigido',
+    'Autorizado coordenador',
+    'Autorizado Gerência',
+    'Autorizado Gerência regional',
+    'Aprovado para pagamento',
+    'Pago',
+    'Reprovada'
+  ];
+  router: any;
+  isModalVisible = false;
+
+  openNovo() {
+    this.router.navigate(['/pderp/financeiro/despesas/nova-despesa']);  }
+
+  
 
 }
