5e120ea705643c9de380bd04484043554029106e
Paulo Martins <paulomart90@gmail.com>
2024-11-21 12:43:46 -0300

    feat: inclusão de nova rota.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,22 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
 import { DespesaComponent } from './router/financeiro/despesa/despesa.component';
+import { NovaDespesaComponent } from './router/financeiro/despesa/modals/nova-despesa/nova-despesa.component';
+import { SenhaComponent } from './router/administracao-acesso/senha/senha.component';
 
 export const routes: Routes = [
 
   { path: '', redirectTo: 'pderp/inicio', pathMatch: 'full' },
   { path: 'pderp/inicio', component: LayoutComponent },
 
+  { path: 'pderp/acesso/senha',
+    component: SenhaComponent},
+
+
   { path: 'pderp/financeiro/despesas',
     component: DespesaComponent},
+    { path: 'pderp/financeiro/despesas/nova-despesa',
+      component: NovaDespesaComponent},
 
 
 ];
