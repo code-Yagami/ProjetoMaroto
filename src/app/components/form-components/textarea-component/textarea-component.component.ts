c0ad2e9f66175a09c0f5ca836899e3bf0e24eb1e
Leandro Ribeiro <leandro.ribeiro@pdcase.com.br>
2024-11-25 14:20:20 -0300

    feat: adicionando componente para campo de area de texto

---
+++ src/app/components/form-components/textarea-component/textarea-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-textarea-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './textarea-component.component.html',
+  styleUrl: './textarea-component.component.scss'
+})
+export class TextareaComponentComponent {
+
+}
