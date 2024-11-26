cada3d9c1a37dcb1b623e0f03d710d9d550edcb3
Leandro Ribeiro <leandro.ribeiro@pdcase.com.br>
2024-11-26 12:19:08 -0300

    feat: Adicionando configuração para ts da tela de adiantamentos

--- src/app/router/pagamentos/adiantamentos/adiantamentos.component.ts
+++ src/app/router/pagamentos/adiantamentos/adiantamentos.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { ButtonComponentComponent } from '../../../components/form-components/button-component/button-component.component';
+import { ModalFormComponent } from '../../../components/form-components/modal-form/modal-form.component';
+import { InputComponentComponent } from '../../../components/form-components/input-component/input-component.component';
+import { TextareaComponentComponent } from '../../../components/form-components/textarea-component/textarea-component.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-adiantamentos',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponentComponent, ModalFormComponent, InputComponentComponent, TextareaComponentComponent, CommonModule,],
   templateUrl: './adiantamentos.component.html',
-  styleUrl: './adiantamentos.component.scss'
+  styleUrl: './adiantamentos.component.scss',
 })
 export class AdiantamentosComponent {
+  mostrarMensagem: boolean = false;
+  tipoAlerta!: string;
+  textoMensagem!: string;
+  testeAlerta: number = 1;
 
+  isModalVisible = false;
+
+  openModal() {
+    this.isModalVisible = true;
+  }
+
+  closeModal() {
+    this.isModalVisible = false;
+  }
+
+  onModalConfirm() {
+    console.log('Modal confirmado!');
+
+    // teste da mensagem de alerta
+    if (this.testeAlerta == 1) {
+
+      this.tipoAlerta = 'sucesso';
+      this.textoMensagem = 'Salvo com sucesso!';
+    } else {
+      this.tipoAlerta = 'erro';
+      this.textoMensagem = 'Erro ao salvar!';
+    }
+
+    this.mostrarMensagem = true;
+    setTimeout(() => {
+      this.mostrarMensagem = false;
+    }, 5000);
+  }
 }
