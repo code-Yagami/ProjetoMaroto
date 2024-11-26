56cb2b50c5341ed1c5d4cb3d35d6df427825a6bb
Leandro Ribeiro <leandro.ribeiro@pdcase.com.br>
2024-11-26 12:14:00 -0300

    feat: Adicionando rota para tela de adiantamentos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { DespesaComponent } from './router/financeiro/despesa/despesa.component'
 import { NovaDespesaComponent } from './router/financeiro/despesa/modals/nova-despesa/nova-despesa.component';
 import { SenhaComponent } from './router/administracao-acesso/senha/senha.component';
 import { InicioComponent } from './router/inicio/inicio/inicio.component';
+import { AdiantamentosComponent } from './router/pagamentos/adiantamentos/adiantamentos.component';
 
 export const routes: Routes = [
   {
@@ -12,7 +13,8 @@ export const routes: Routes = [
     children: [
       { path: '', redirectTo: 'pderp/inicio', pathMatch: 'full' },
       { path: 'pderp/financeiro/despesas', component: DespesaComponent },
-      { path: 'pderp/acesso/senha', component: SenhaComponent }
+      { path: 'pderp/acesso/senha', component: SenhaComponent },
+      { path: 'pderp/pagamento/andiantamentos', component: AdiantamentosComponent}
     ]
   }
 ];
