5010d5b09fa1833cc9c78ebd7fdc95bc54cb3a42
Leandro Ribeiro <leandro.ribeiro@pdcase.com.br>
2024-11-25 21:53:38 -0300

    fix: ajustes na tela de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
 import { LoginComponent } from './components/login/login.component';
 import { DespesaComponent } from './router/financeiro/despesa/despesa.component';
-import { NovaDespesaComponent } from './router/financeiro/despesa/modals/nova-despesa/nova-despesa.component';
 import { SenhaComponent } from './router/administracao-acesso/senha/senha.component';
 import { InicioComponent } from './router/inicio/inicio/inicio.component';
 
