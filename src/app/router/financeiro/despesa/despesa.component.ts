f873edf90f39fb6f2bc8af7ff69175c7a0a07f9d
Paulo Martins <paulomart90@gmail.com>
2024-11-26 11:25:11 -0300

    feat: ajuste na tela de despesa

--- src/app/router/financeiro/despesa/despesa.component.ts
+++ src/app/router/financeiro/despesa/despesa.component.ts
@@ -5,11 +5,14 @@ import { CardComponent } from '../../../components/form-components/card/card.com
 import { TableComponent } from '../../../components/form-components/table/table.component';
 import { LayoutComponent } from '../../../components/layout/layout.component';
 import { NovaDespesaComponent } from './modals/nova-despesa/nova-despesa.component';
+import { NumberComponentComponent } from "../../../components/form-components/number-component/number-component.component";
+import { DataComponentComponent } from "../../../components/form-components/data-component/data-component.component";
+import { InteractiveTableComponentComponent } from "../../../components/form-components/interactive-table-component/interactive-table-component.component";
 
 @Component({
   selector: 'app-despesa',
   standalone: true,
-  imports: [LayoutComponent, RouterLink, CardComponent, TableComponent, NovaDespesaComponent],
+  imports: [LayoutComponent, RouterLink, CardComponent, TableComponent, NovaDespesaComponent, NumberComponentComponent, DataComponentComponent, InteractiveTableComponentComponent],
   templateUrl: './despesa.component.html',
   styleUrl: './despesa.component.scss'
 })
