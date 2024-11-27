93449008d02a467b3047863cd2d67220283f93e9
7BRUNOY <bruno.souza@pdcase.com.br>
2024-11-27 19:15:09 -0300

    feat/fix

--- src/app/router/colaboradores/aniversarios/aniversarios.component.ts
+++ src/app/router/colaboradores/aniversarios/aniversarios.component.ts
@@ -1,71 +1,29 @@
-interface Anniversary {
-  birthday: string;
-  fullName: string;
-  email: string;
-  costCenter: string;
-  city: string;
+import { Component } from '@angular/core';
+import { InputComponentComponent } from "../../../components/form-components/input-component/input-component.component";
+import { ButtonComponentComponent } from "../../../components/form-components/button-component/button-component.component";
+import { CardComponent } from "../../../components/form-components/card/card.component";
+import { DataComponentComponent } from "../../../components/form-components/data-component/data-component.component";
+import { NumberComponentComponent } from "../../../components/form-components/number-component/number-component.component";
+import { TableComponent } from "../../../components/form-components/table/table.component";
+@Component({
+  selector: 'app-aniversarios',
+  standalone: true,
+  imports: [InputComponentComponent, InputComponentComponent, ButtonComponentComponent, CardComponent, DataComponentComponent, NumberComponentComponent, TableComponent],
+  templateUrl: './aniversarios.component.html',
+  styleUrl: './aniversarios.component.scss'
+})
+export class RelatoriosComponent {
+  selectedCity: string = '';
+  selectedMonth: string = '';
+
+  cidades: string[] = [
+    'São Paulo', 'Rio de Janeiro', 'Belo Horizonte', 'Salvador', 'Porto Alegre',
+    'Curitiba', 'Fortaleza', 'Recife', 'Manaus', 'Brasília', 'Belém', 'Goiânia'
+  ];
+
+  meses: string[] = [
+    'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+    'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+  ];
 }
 
-const anniversaryData: Anniversary[] = [
-  {
-      birthday: '01/11',
-      fullName: 'Jéssica Espíndola Fontoura',
-      email: 'jessica.fontoura@pdcase.com.br',
-      costCenter: 'PDI\\DRO-01\\MG\\PRODFINAN\\',
-      city: 'Belo Horizonte'
-  },
-  {
-      birthday: '02/11',
-      fullName: 'Hernan Verches',
-      email: 'hernan.verches@pdcase.com.br',
-      costCenter: 'PDI\\DRO-02\\ES\\BANESTES\\',
-      city: 'Vitória'
-  },
-  {
-      birthday: '03/11',
-      fullName: 'Jackson Willian Silva Agostinho',
-      email: 'jackson.agostinho@pdcase.com.br',
-      costCenter: 'PDI\\DRO-02\\ES\\BANESTES\\',
-      city: 'Vitória'
-  },
-];
-
-const searchBtn = document.getElementById('search-btn') as HTMLButtonElement;
-const anniversaryTableBody = document.querySelector('#anniversary-table tbody') as HTMLTableSectionElement;
-
-searchBtn.addEventListener('click', () => {
-  const name = (document.getElementById('name') as HTMLInputElement).value.toLowerCase();
-  const city = (document.getElementById('city') as HTMLSelectElement).value;
-  const month = (document.getElementById('month') as HTMLSelectElement).value;
-  const costCenter = (document.getElementById('cost-center') as HTMLSelectElement).value;
-
-  const filteredData = anniversaryData.filter((data) => {
-      return (
-          (name === '' || data.fullName.toLowerCase().includes(name)) &&
-          (city === 'todos' || data.city.toLowerCase() === city) &&
-          (month === 'novembro' || data.birthday.includes(month)) &&
-          (costCenter === 'todos' || data.costCenter.includes(costCenter))
-      );
-  });
-
-  updateTable(filteredData);
-});
-
-function updateTable(data: Anniversary[]) {
-  anniversaryTableBody.innerHTML = '';
-
-  data.forEach((anniversary) => {
-      const row = document.createElement('tr');
-
-      row.innerHTML = `
-          <td>${anniversary.birthday}</td>
-          <td>${anniversary.fullName}</td>
-          <td>${anniversary.email}</td>
-          <td>${anniversary.costCenter}</td>
-          <td>${anniversary.city}</td>
-      `;
-
-      anniversaryTableBody.appendChild(row);
-  });
-}
-updateTable(anniversaryData);
--- src/app/router/relatorios/aniversarios/aniversarios.component.ts
+++ src/app/router/relatorios/aniversarios/aniversarios.component.ts
@@ -2,63 +2,27 @@ import { Component } from '@angular/core';
 import { InputComponentComponent } from "../../../components/form-components/input-component/input-component.component";
 import { ButtonComponentComponent } from "../../../components/form-components/button-component/button-component.component";
 import { CardComponent } from "../../../components/form-components/card/card.component";
-
+import { DataComponentComponent } from "../../../components/form-components/data-component/data-component.component";
+import { NumberComponentComponent } from "../../../components/form-components/number-component/number-component.component";
+import { TableComponent } from "../../../components/form-components/table/table.component";
 @Component({
   selector: 'app-aniversarios',
   standalone: true,
-  imports: [InputComponentComponent, InputComponentComponent, ButtonComponentComponent, CardComponent],
+  imports: [InputComponentComponent, ButtonComponentComponent, CardComponent, DataComponentComponent, NumberComponentComponent, TableComponent],
   templateUrl: './aniversarios.component.html',
-  styleUrl: './aniversarios.component.scss'
+  styleUrls: ['./aniversarios.component.scss']
 })
 export class AniversariosComponent {
+  selectedCity: string = '';
+  selectedMonth: string = '';
 
-  cidadeSelecionada: string | null = "";
-  mesSelecionado: string | null = "";
-  centroSelecionado: string | null = "";
+  cidades: string[] = [
+    'São Paulo', 'Rio de Janeiro', 'Belo Horizonte', 'Salvador', 'Porto Alegre',
+    'Curitiba', 'Fortaleza', 'Recife', 'Manaus', 'Brasília', 'Belém', 'Goiânia'
+  ];
 
-  selecionaCidade(valor: string) {
-    this.cidadeSelecionada = valor;
-    console.log("Valor atualizado no componente pai:", this.cidadeSelecionada);
-  }
-
-  selecionaMes(valor: string) {
-    this.mesSelecionado = valor;
-    console.log("Valor atualizado no componente pai:", this.mesSelecionado);
-  }
-
-  selecionaCentro(valor: string) {
-    this.centroSelecionado = valor;
-    console.log("Valor atualizado no componente pai:", this.centroSelecionado);
-  }
-
-  listaCidade = [
-    "Belém",
-    "São Paulo",
-    "Rio de Janeiro",
-    "São Luiz",
-    "Belo Horizonte"
-  ]
-
-  listaMes = [
-    "Janeiro",
-    "Fevereiro",
-    "Março",
-    "Abril",
-    "Maio",
-    "Junho",
-    "Julho",
-    "Agosto",
-    "Setembro",
-    "Outubro",
-    "Novembro",
-    "Dezembro"
-  ]
-
-  listaCentroCusto = [
-    "PD\\",
-    "PD\\DRO-01\\",
-    "PD\\DRO-01\\AL\\",
-    "PD\\DRO-01\\AL\\SEFAZ-AL\\"
-  ]
-
+  meses: string[] = [
+    'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+    'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+  ];
 }
