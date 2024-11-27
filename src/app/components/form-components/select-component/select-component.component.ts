e7e9bff6cbba4b087d215726b26843e4081435c1
Leandro Ribeiro <leandro.ribeiro@pdcase.com.br>
2024-11-27 00:47:56 -0300

    fix: Ajuste de componente para desenvolvimento da tela de emprestimos

---
+++ src/app/components/form-components/select-component/select-component.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+ 
+@Component({
+  selector: 'app-select-component',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './select-component.component.html',
+  styleUrl: './select-component.component.scss'
+})
+export class SelectComponentComponent {
+  @Input() listaRender!: any[] | null;
+  @Output() seleciona = new EventEmitter<any>();
+ 
+  onSelecionado(event: Event) {
+    const valorSelecionado = (event.target as HTMLSelectElement).value;
+    this.seleciona.emit(valorSelecionado);
+  }
+}
