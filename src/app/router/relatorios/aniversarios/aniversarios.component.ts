925798c1f0f18484cfd17b12279f4d39e438f649
7BRUNOY <bruno.souza@pdcase.com.br>
2024-11-27 12:11:03 -0300

    feat/fix: adicionados novos componentes, relatorios e colaboradores > aniversarios/aniversariantes

---
+++ src/app/router/relatorios/aniversarios/aniversarios.component.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+import { InputComponentComponent } from "../../../components/form-components/input-component/input-component.component";
+import { SelectComponentComponent } from "../../../components/form-components/select-component/select-component.component";
+import { ButtonComponentComponent } from "../../../components/form-components/button-component/button-component.component";
+import { CardComponent } from "../../../components/form-components/card/card.component";
+
+@Component({
+  selector: 'app-aniversarios',
+  standalone: true,
+  imports: [InputComponentComponent, SelectComponentComponent, ButtonComponentComponent, CardComponent],
+  templateUrl: './aniversarios.component.html',
+  styleUrl: './aniversarios.component.scss'
+})
+export class AniversariosComponent {
+
+  cidadeSelecionada: string | null = "";
+  mesSelecionado: string | null = "";
+  centroSelecionado: string | null = "";
+
+  selecionaCidade(valor: string) {
+    this.cidadeSelecionada = valor;
+    console.log("Valor atualizado no componente pai:", this.cidadeSelecionada);
+  }
+
+  selecionaMes(valor: string) {
+    this.mesSelecionado = valor;
+    console.log("Valor atualizado no componente pai:", this.mesSelecionado);
+  }
+
+  selecionaCentro(valor: string) {
+    this.centroSelecionado = valor;
+    console.log("Valor atualizado no componente pai:", this.centroSelecionado);
+  }
+
+  listaCidade = [
+    "Belém",
+    "São Paulo",
+    "Rio de Janeiro",
+    "São Luiz",
+    "Belo Horizonte"
+  ]
+
+  listaMes = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro"
+  ]
+
+  listaCentroCusto = [
+    "PD\\",
+    "PD\\DRO-01\\",
+    "PD\\DRO-01\\AL\\",
+    "PD\\DRO-01\\AL\\SEFAZ-AL\\"
+  ]
+
+}
