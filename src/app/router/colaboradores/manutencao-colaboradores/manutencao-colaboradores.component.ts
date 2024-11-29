149bdc80c3642b8ff1680dee7baf3f6cc5467f3d
Leandro Ribeiro <leandro.ribeiro@pdcase.com.br>
2024-11-29 08:46:05 -0300

    feat: Adicionando componente da tela de manutenção colaboradores

---
+++ src/app/router/colaboradores/manutencao-colaboradores/manutencao-colaboradores.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manutencao-colaboradores',
+  standalone: true,
+  imports: [],
+  templateUrl: './manutencao-colaboradores.component.html',
+  styleUrl: './manutencao-colaboradores.component.scss'
+})
+export class ManutencaoColaboradoresComponent {
+
+}
