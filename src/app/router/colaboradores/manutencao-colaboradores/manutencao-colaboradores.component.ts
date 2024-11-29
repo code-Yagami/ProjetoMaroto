67e1d1521fddde2c5115ef41dc1da5942b76eaeb
Leandro Ribeiro <leandro.ribeiro@pdcase.com.br>
2024-11-29 09:36:07 -0300

    feat: Adicionando configurações iniciais de ts para tela de manutenção colaboradores

--- src/app/router/colaboradores/manutencao-colaboradores/manutencao-colaboradores.component.ts
+++ src/app/router/colaboradores/manutencao-colaboradores/manutencao-colaboradores.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../../components/form-components/card/card.component';
+import { InputComponentComponent } from '../../../components/form-components/input-component/input-component.component';
 
 @Component({
   selector: 'app-manutencao-colaboradores',
   standalone: true,
-  imports: [],
+  imports: [CardComponent,InputComponentComponent],
   templateUrl: './manutencao-colaboradores.component.html',
   styleUrl: './manutencao-colaboradores.component.scss'
 })
