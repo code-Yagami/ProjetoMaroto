0324ee87b8f44fe85f9d8ae2715491f773456226
Leandro Ribeiro <leandro.ribeiro@pdcase.com.br>
2024-12-02 14:27:20 -0300

    fix: Ajuste no botão da tabela na tela de manutenção colaboradores

--- src/app/router/colaboradores/manutencao-colaboradores/manutencao-colaboradores.component.ts
+++ src/app/router/colaboradores/manutencao-colaboradores/manutencao-colaboradores.component.ts
@@ -94,7 +94,7 @@ export class ManutencaoColaboradoresComponent {
   showEdit = true;
   showDelete = false;
   showView = true;
-  editButtonTitle = 'Imprimir';
+  editButtonTitle = 'Editar';
   deleteButtonTitle = 'Deletar';
   viewButtonTitle = 'Visualizar';
 
