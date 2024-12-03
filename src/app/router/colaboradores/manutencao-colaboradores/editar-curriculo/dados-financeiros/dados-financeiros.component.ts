23598ac0650753ee192360aa2c0d6037c16e4fc5
Paulo Martins <paulomart90@gmail.com>
2024-12-03 15:53:02 -0300

    feat: Implementação da tela dados financeiros

--- src/app/router/colaboradores/manutencao-colaboradores/editar-curriculo/dados-financeiros/dados-financeiros.component.ts
+++ src/app/router/colaboradores/manutencao-colaboradores/editar-curriculo/dados-financeiros/dados-financeiros.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CardComponent } from "../../../../../components/form-components/card/card.component";
-import { NavTabsComponent } from "../../../../../components/form-components/nav-tabs/nav-tabs.component";
+import { NavTabsComponent, Tab } from "../../../../../components/form-components/nav-tabs/nav-tabs.component";
 
 @Component({
   selector: 'app-dados-financeiros',
@@ -10,5 +10,21 @@ import { NavTabsComponent } from "../../../../../components/form-components/nav-
   styleUrl: './dados-financeiros.component.scss'
 })
 export class DadosFinanceirosComponent {
+
+
+  tabs: Tab[] = [
+    { id: 'personal-data', label: 'Dados Pessoais', link: '/dados-pessoais' },
+    { id: 'financial-data', label: 'Dados Financeiros', link: 'dados-financeiros'  },
+    { id: 'languages', label: 'Idiomas', link: ''  },
+    { id: 'positions', label: 'Cargos Pretendidos', link: ''  },
+    { id: 'previous-employment', label: 'Emprego Anteriores', link: ''  },
+    { id: 'skills', label: 'Conhecimentos Específicos', link: ''  },
+    { id: 'search-employees', label: 'Pesquisar Colaboradores', link: ''  },
+    { id: 'education', label: 'Escolaridade', link: ''  },
+    { id: 'courses', label: 'Cursos', link: ''  },
+    { id: 'mini-cv', label: 'Mini-Currículo', link: ''  },
+    { id: 'certifications', label: 'Certificações' , link: '' },
+    { id: 'upload-cv', label: 'Anexar Currículo', link: ''  }
+  ];
 
 }
