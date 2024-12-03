502b93ef3ba000420ea176374a9b558d70ce8e75
Leandro Ribeiro <leandro.ribeiro@pdcase.com.br>
2024-12-03 10:54:59 -0300

    feat: Adicionando configuração de ts para botão de editar

--- src/app/components/form-components/table/table.component.ts
+++ src/app/components/form-components/table/table.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, RouterLink],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss'
 })
@@ -21,8 +22,11 @@ export class TableComponent {
   @Input() showViewButton: boolean = false; // Controla a exibição do botão Visualizar
   @Input() viewButtonTitle: string = 'Visualizar'; // Título do botão Visualizar
 
+  router: any;
+
   onEdit(row: any) {
     console.log('Editando:', row);
+    this.router.navigate(['/pderp/colaboradores/manutencao-colaboradores/editar-curriculo']);
   }
 
   onDelete(row: any) {
