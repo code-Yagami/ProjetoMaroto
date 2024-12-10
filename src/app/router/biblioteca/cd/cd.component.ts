dd6540b9ad8554f453a772bebdf4c473db609bfc
7BRUNOY <bruno.souza@pdcase.com.br>
2024-12-10 10:01:31 -0300

    fix: adicionado o mock

--- src/app/router/biblioteca/cd/cd.component.ts
+++ src/app/router/biblioteca/cd/cd.component.ts
@@ -13,17 +13,36 @@ import { TableComponent } from '../../../components/form-components/table/table.
 })
 export class CdComponent {
 
-  statusOptions = [
-    'Disponível',
-    'Não Disponível',
-    'Em Atraso',
+  tableData = [
+    {
+      nome: 'Expression Studio 4 Premium',
+      serial: 'C44HB-2VGYW-GXQKM-WKCVH-6YYJ6',
+      numCds: 1,
+      linguagem: 'eng',
+      tipo: 'Aplicações',
+      status: 'Disponível',
+      acoes: 'Solicitar',
+    },
+    {
+      nome: 'Office Professional 2007',
+      serial: 'VKB8P-6FX4J-7JRYW-897H6-6XPHQ',
+      numCds: 1,
+      linguagem: 'br / eng',
+      tipo: 'Aplicações',
+      status: 'Não Disponível',
+      acoes: 'Lista Espera',
+    },
+    {
+      nome: 'Office Project Professional 2007',
+      serial: 'TJXDC-BBPWV-2C7BP-R69VR-VC3DQ',
+      numCds: 1,
+      linguagem: 'br / eng',
+      tipo: 'Aplicações',
+      status: 'Não Disponível',
+      acoes: 'Lista Espera',
+    },
   ];
-
-  tipoOptions = [
-    'Aplicações',
-    'Desenvolvimento',
-    'Servidores',
-    'Sistema Operacional',
-    'Outros',
-  ];
+  linguagemOptions!: any[];
+  tipoOptions!: any[];
+  statusOptions!: any[];
 }
